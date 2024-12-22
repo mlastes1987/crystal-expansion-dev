cf2ac1c5dbcddabfff347c47b2240796c51a3089
MARK\mlast <mlastes1987@gmail.com>
2024-12-21 20:19:10 -0500

    Final clean up for Elm's Aide NPC code, will confirm during playthrough.

--- maps/Route42EcruteakGate.asm
+++ maps/Route42EcruteakGate.asm
@@ -58,8 +58,9 @@ AideTextGreeting:
 
 	para "If you have caught 20"
 	line "#MON I'm supposed to"
-	cont "give you an EXP."
-	cont "SHARE."
+	
+	para "give you an EXP."
+	line "SHARE."
 	
 	para "Have you caught 20 #MON?"
 	done
